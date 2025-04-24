a2390c50be0d1a630cc0384871fcc7549dc11fda
Yaminianitha19 <yaminianitha22@gmail.com>
2025-04-24 12:53:56 +0530

    installed all the requirements

---
+++ scrape-flow/app/(dashboard)/home/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function layout({children}: {children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen">
+        <div className="flex flex-col flex-1 min-h-screen">
+
+        </div>
+        </div>
+    
+  )
+}
+
+export default layout

8da420b290f2b6079aa34ff8fcea184d8f6b8445
Yaminianitha19 <yaminianitha22@gmail.com>
2025-04-24 14:05:46 +0530

    16:55

--- scrape-flow/components/sidebar.tsx
+++ scrape-flow/components/sidebar.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
+import { CoinsIcon, HomeIcon, Layers2Icon, Link, ShieldCheckIcon } from 'lucide-react';
 import React from 'react'
+import Logo from './logo';
 
 const routes = [
     {
@@ -29,7 +30,17 @@ function DesktopSidebar() {
   return (
     <div className = "hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
         <div className="flex items-center justify-center gap-2 border-b-[1px]border-separate p-4">
-           < logo/>
+            <Logo />
+        </div>
+        <div className="flex flex-col p-2">
+            {routes.map((route) => (
+                <Link key={route.href}>
+                    <route.icon size={20}/>
+                    {route.label}
+                </Link>
+            ))}
+            
+        
         </div>
         </div>
   )
